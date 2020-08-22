913f22072ea26fab8fa8c2c8bdbf91762c47b61c
lmjcbs <hello@liamjacobs.dev>
2020-08-22 18:38:30 +0100

    create User table

--- src/mikro-orm.config.ts
+++ src/mikro-orm.config.ts
@@ -1,4 +1,5 @@
 import { __prod__ } from './constants';
+import { User } from './entities/User';
 import { League } from './entities/League';
 import { MikroORM } from '@mikro-orm/core';
 import path from 'path';
@@ -8,7 +9,7 @@ export default {
         path: path.join(__dirname, './migrations'),
         pattern: /^[\w-]+\d+\.[tj]s$/,
     },
-    entities: [League],
+    entities: [User, League],
     dbName: 'fplking',
     type: 'postgresql',
     debug: !__prod__,
