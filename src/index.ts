616bdc499f032a649af2580294e3f543d9fb9c20
lmjcbs <hello@liamjacobs.dev>
2020-08-22 18:11:24 +0100

    setup express app with league resolver

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,31 @@
+import 'reflect-metadata';
 import { MikroORM } from '@mikro-orm/core';
 import { __prod__ } from './constants';
-import { League } from './entities/League';
 import microConfig from './mikro-orm.config';
+import express from 'express';
+import { ApolloServer } from 'apollo-server-express';
+import { buildSchema } from 'type-graphql';
+import { LeagueResolver } from './resolvers/league';
 
 const main = async () => {
     const orm = await MikroORM.init(microConfig);
     await orm.getMigrator().up();
 
-    const league = orm.em.create(League, { name: 'my first league' });
-    await orm.em.persistAndFlush(league);
+    const app = express();
+
+    const apolloServer = new ApolloServer({
+        schema: await buildSchema({
+            resolvers: [LeagueResolver],
+            validate: false,
+        }),
+        context: () => ({ em: orm.em }),
+    });
+
+    apolloServer.applyMiddleware({ app });
+
+    app.listen(4000, () => {
+        console.log('Server started on localhost:4000');
+    });
 };
 
 main().catch((err) => {
