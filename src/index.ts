dae01197c1d64909ac2bb47a7ef0ac97944f5e07
lmjcbs <hello@liamjacobs.dev>
2020-08-23 16:12:10 +0100

    add redis store for sessions

--- src/index.ts
+++ src/index.ts
@@ -7,19 +7,45 @@ import { ApolloServer } from 'apollo-server-express';
 import { buildSchema } from 'type-graphql';
 import { UserResolver } from './resolvers/user';
 import { LeagueResolver } from './resolvers/league';
+import redis from 'redis';
+import session from 'express-session';
+import connectRedis from 'connect-redis';
 
 const main = async () => {
+    require('dotenv').config();
     const orm = await MikroORM.init(microConfig);
     await orm.getMigrator().up();
 
     const app = express();
+
+    const RedisStore = connectRedis(session);
+    const redisClient = redis.createClient();
+
+    app.use(
+        session({
+            name: 'qid',
+            store: new RedisStore({
+                client: redisClient,
+                disableTouch: true,
+            }),
+            cookie: {
+                maxAge: 1000 * 60 * 60 * 24 * 365, // 1 Year timeout
+                httpOnly: true,
+                sameSite: 'lax', //csrf
+                secure: __prod__, //cookie only works in https
+            },
+            saveUninitialized: false,
+            secret: process.env.SESSION_SECRET!,
+            resave: false,
+        })
+    );
 
     const apolloServer = new ApolloServer({
         schema: await buildSchema({
             resolvers: [UserResolver, LeagueResolver],
             validate: false,
         }),
-        context: () => ({ em: orm.em }),
+        context: ({ req, res }) => ({ em: orm.em, req, res }),
     });
 
     apolloServer.applyMiddleware({ app });
