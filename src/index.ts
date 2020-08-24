4ff18aa5bc8e00985be2dea503eb074580e83d8b
lmjcbs <hello@liamjacobs.dev>
2020-08-24 12:57:32 +0100

    add app cors preferences

--- src/index.ts
+++ src/index.ts
@@ -10,6 +10,7 @@ import { LeagueResolver } from './resolvers/league';
 import redis from 'redis';
 import session from 'express-session';
 import connectRedis from 'connect-redis';
+import cors from 'cors';
 
 const main = async () => {
     require('dotenv').config();
@@ -20,6 +21,13 @@ const main = async () => {
 
     const RedisStore = connectRedis(session);
     const redisClient = redis.createClient();
+
+    app.use(
+        cors({
+            origin: 'http://localhost:3000',
+            credentials: true,
+        })
+    );
 
     app.use(
         session({
@@ -48,7 +56,10 @@ const main = async () => {
         context: ({ req, res }) => ({ em: orm.em, req, res }),
     });
 
-    apolloServer.applyMiddleware({ app });
+    apolloServer.applyMiddleware({
+        app,
+        cors: false,
+    });
 
     app.listen(4000, () => {
         console.log('Server started on localhost:4000');
